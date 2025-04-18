1720d1da88b37206107ed7b5bfe46a5af8829509
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2025-04-18 12:18:19 +0530

    project

---
+++ Day7/Lms/src/com/java/lms/model/LeaveDetails.java
@@ -0,0 +1,17 @@
+package com.java.lms.model;
+
+import java.util.Date;
+
+public class LeaveDetails {
+
+	private int leaveId;
+	private int empId;
+	private Date leaveStartDate;
+	private Date leaveEndDate;
+	private int noOfDays;
+	private String leaveReason;
+	private LeaveType leaveType;
+	private LeaveStatus leaveStatus;
+	
+	
+}
