50efe5e86b8abb5a861116fd69f862e5d00c077f
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2025-05-07 12:21:35 +0530

    code

--- Day18/LibraryExample/src/main/java/com/java/lib/dao/LibraryDao.java
+++ Day18/LibraryExample/src/main/java/com/java/lib/dao/LibraryDao.java
@@ -1,11 +1,14 @@
 package com.java.lib.dao;
 
 import java.sql.SQLException;
+import java.util.List;
 
+import com.java.lib.model.Books;
 import com.java.lib.model.LibUsers;
 
 public interface LibraryDao {
 
 	String createUser(LibUsers libUsers) throws ClassNotFoundException, SQLException;
 	int login(LibUsers libUsers) throws ClassNotFoundException, SQLException;
+	List<Books> searchBooks(String searchType, String searchValue) throws ClassNotFoundException, SQLException;
 }
--- Day18/LibraryExample/src/main/java/com/java/lib/dao/LibraryDaoImpl.java
+++ Day18/LibraryExample/src/main/java/com/java/lib/dao/LibraryDaoImpl.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.java.lib.model.Books;
 import com.java.lib.model.LibUsers;
 import com.java.lib.util.ConnectionHelper;
 import com.java.lib.util.EncryptPassword;
@@ -39,6 +42,43 @@ public class LibraryDaoImpl implements LibraryDao {
 		rs.next();
 		int count= rs.getInt("cnt");
 		return count;
+	}
+
+	@Override
+	public List<Books> searchBooks(String searchType, String searchValue) throws ClassNotFoundException, SQLException {
+		String cmd;
+		boolean isValid = true;
+		if(searchType.equals("id")) {
+			cmd = " SELECT * FROM Books WHERE Id = ? " ;
+		} else if (searchType.equals("bookname")) {
+			cmd = " SELECT * FROM Books Where Name = ?";
+		} else if (searchType.equals("authorname")) {
+			cmd = "SELECT * FROM Books where Author = ?";
+		} else if (searchType.equals("dept")) {
+			cmd = "select * from Books where Dept = ?";
+		} else {
+			isValid = false;
+			cmd = "select * from Books";
+		}
+		connection = ConnectionHelper.getConnection();
+		psmt = connection.prepareStatement(cmd);
+		if (isValid == true) {
+			psmt.setString(1, searchValue);
+		}
+		ResultSet rs = psmt.executeQuery();
+		Books books = null;
+		List<Books> booksList = new ArrayList<Books>();
+		while(rs.next()) {
+			books = new Books();
+			books.setId(rs.getInt("id"));
+			books.setName(rs.getString("name"));
+			books.setAuthor(rs.getString("author"));
+			books.setEdition(rs.getString("edition"));
+			books.setDept(rs.getString("dept"));
+			books.setNoOfBooks(rs.getInt("TotalBooks"));
+			booksList.add(books);
+		}
+		return booksList;
 	}
 
 }
