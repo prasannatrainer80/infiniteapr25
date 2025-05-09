f9cb50e91e0ae43a4a32b1c85cbd3325caadea68
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2025-05-09 10:32:35 +0530

    code

---
+++ Day20/HibExamples/src/main/java/com/java/hib/EmployShowMain.java
@@ -0,0 +1,23 @@
+package com.java.hib;
+
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+public class EmployShowMain {
+
+	public static void main(String[] args) {
+		SessionFactory sf = new AnnotationConfiguration().configure().buildSessionFactory();
+		System.out.println("Database Connected");
+		Session session = sf.openSession();
+		Query query = session.createQuery("from Employ");
+		List<Employ> employList = query.list();
+		for (Employ employ : employList) {
+			System.out.println(employ);
+		}
+	}
+}
