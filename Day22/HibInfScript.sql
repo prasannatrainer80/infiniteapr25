09b205272ee330232567d34392a7d6d9676cc5ae
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2025-05-12 10:42:15 +0530

    code

---
+++ Day22/HibInfScript.sql
@@ -0,0 +1,25 @@
+drop database if exists infinitehib;
+
+create database infinitehib;
+
+use infinitehib;
+
+create table Employee
+(
+   empid int primary key auto_increment,
+   firstName varchar(30),
+   lastName varchar(30),
+   userName varchar(30),
+   passCode varchar(255),
+   email varchar(30),
+   status varchar(30) default 'inactive'
+);
+
+create table Otp
+(
+   otpid int primary key auto_increment,
+   userName varchar(30),
+   otpNo int,
+   npassword varchar(50),
+   status varchar(30) default 'inactive'
+)
