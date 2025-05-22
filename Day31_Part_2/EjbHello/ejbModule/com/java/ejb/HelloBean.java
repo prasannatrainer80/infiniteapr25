fdd529a77ccccf8b392236c05ecb938f3a3fb36c
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2025-05-22 15:02:00 +0530

    code

---
+++ Day31_Part_2/EjbHello/ejbModule/com/java/ejb/HelloBean.java
@@ -0,0 +1,27 @@
+package com.java.ejb;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class HelloBean
+ */
+@Stateless
+@Remote(HelloBeanRemote.class)
+public class HelloBean implements HelloBeanRemote {
+
+    /**
+     * Default constructor. 
+     */
+    public HelloBean() {
+        // TODO Auto-generated constructor stub
+    }
+
+	@Override
+	public String sayHello() {
+		// TODO Auto-generated method stub
+		return "Welcome to EJB Programming from Infinite Batch...";
+	}
+
+}
