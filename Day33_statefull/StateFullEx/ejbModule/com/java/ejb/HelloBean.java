ec4a5825d87524c5cd400b976f45f8afc2dce956
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2025-05-26 15:49:30 +0530

    code

---
+++ Day33_statefull/StateFullEx/ejbModule/com/java/ejb/HelloBean.java
@@ -0,0 +1,27 @@
+package com.java.ejb;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Remote;
+import javax.ejb.Stateful;
+
+/**
+ * Session Bean implementation class HelloBean
+ */
+@Stateful
+@Remote(HelloBeanRemote.class)
+public class HelloBean implements HelloBeanRemote {
+
+    /**
+     * Default constructor. 
+     */
+    public HelloBean() {
+        // TODO Auto-generated constructor stub
+    }
+
+	@Override
+	public String sayHello() {
+		// TODO Auto-generated method stub
+		return "Welcome to Statefull Beans...";
+	}
+
+}
